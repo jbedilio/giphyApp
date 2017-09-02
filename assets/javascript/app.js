25c98fe7243efd16aebff42687d2ba3da1c71851
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-01 21:16:25 -0400

    finished the function makeButtons(); and pseudo coded my program

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
+//array of giphs
+var giphs = [ "puppy"]; //"dog", "kitten", "cat", "hamster", "bird", "turtle", "rabbit", "horse", "goat"];
 
-var giphs = [ "puppy", "dog", "kitten", "cat", "hamster", "bird", "turtle", "rabbit", "horse", "goat"];
-
-
-
-    //for (var i = 0; i < giphs.length; i++) {
-        //console.log(giphs[i]);
-        //}
-        
+//function to create giph buttons
 function makeButtons() {
+    //deletes the #giphs-view div each time a new giph is added, preventing repeat giph buttons 
+    $('#giph-view').empty();
     
     for (var i = 0; i < giphs.length; i++) {
-        console.log(giphs[i]);
+        console.log("first", giphs[i]);
+        var $btn = $('<button>').css({"margin-top": "5px"});
+        console.log("second", $btn);
+        $btn.addClass('giphs');
+        console.log("third", $btn);
+        $btn.attr('data-name', giphs[i]);
+        console.log("forth", $btn);
+        $btn.text(giphs[i]);
+        console.log('fifth', $btn);
+        $('#giph-view').append($btn);
+        console.log("fifth", '#giph-view');
         }
     }
 
