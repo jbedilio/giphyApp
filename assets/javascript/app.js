2329c9052f69dc71259d4d6a5693ed3c06c3e1b6
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-01 22:55:02 -0400

    developed the ajax queryURL and pseudo coded

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,45 @@
 $(document).ready(function() {
-//array of giphs
+//starting array of giphs
 var giphs = [ "puppy"]; //"dog", "kitten", "cat", "hamster", "bird", "turtle", "rabbit", "horse", "goat"];
+
+function showGiph() {
+    var animal = $(this).attr('data-name')
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=1e1310e23de94135a921363415524870&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        console.log('seventh', response);
+    })
+
+
+
+
+}
 
 //function to create giph buttons
 function makeButtons() {
     //deletes the #giphs-view div each time a new giph is added, preventing repeat giph buttons 
     $('#giph-view').empty();
-    
+    //for loop that loops through the entire array, rendering a button for each index of the array
     for (var i = 0; i < giphs.length; i++) {
         console.log("first", giphs[i]);
+        //dynamically creating a button through JQuery with a css class for styling
         var $btn = $('<button>').css({"margin-top": "5px"});
         console.log("second", $btn);
+        //adding the class="giphs" to the newly dynamically created buttons
         $btn.addClass('giphs');
         console.log("third", $btn);
+
         $btn.attr('data-name', giphs[i]);
         console.log("forth", $btn);
+        //gives each button a text lable matching the text of the element in the array
         $btn.text(giphs[i]);
         console.log('fifth', $btn);
+        //locates each button in the div with the id="giph-view"
         $('#giph-view').append($btn);
-        console.log("fifth", '#giph-view');
+        console.log("sixth", $btn);
         }
     }
 
