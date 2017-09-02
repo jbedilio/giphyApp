09662285d2c3c847177a801c3ea42cc1199e3b82
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-02 14:43:41 -0400

    pseudo coding what I'm trying to do and completing the .done function for loop's dynamic rendering of the giphs on the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,18 +2,27 @@ $(document).ready(function() {
 //starting array of giphs
 var giphs = [ "puppy"]; //"dog", "kitten", "cat", "hamster", "bird", "turtle", "rabbit", "horse", "goat"];
 
-function showGiph() {
+function playGiph() {
     //var that holds the button/element value clicked on and concatenates into the queryURL var completing the ajax request
     var giph = $(this).attr('data-name')
-    //var holding the incomplete ajax request, waiting on the giph var to concatenate and complete the request
+    //var holding the incomplete url for the ajax request, waiting on the giph var to concatenate and complete the url
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giph + "&api_key=1e1310e23de94135a921363415524870&limit=10";
-
+    //actual ajax function requesting data from the API with the "GET" method
     $.ajax({
         url: queryURL,
         method: "GET"
+    //.done function instructing the computer to use the response info from the API to carry out the following actions
     }).done(function(response){
         console.log(response);
-    })
+        
+        for(var i = 0; i < response.data.length; i++) {
+            var viewDiv = $('<div class="giph">');
+            console.log(viewDiv);
+            var giphImage = $('<img>');
+            giphImage.attr('src', response.data[i].images.downsized_still.url);
+            console.log(giphImage);
+        } 
+    })  
 
 
 
@@ -23,12 +32,12 @@ function showGiph() {
 //function to create giph buttons
 function makeButtons() {
     //deletes the #giphs-view div each time a new giph is added, preventing repeat giph buttons 
-    $('#giph-view').empty();
+    $('#giph-buttons').empty();
     //for loop that loops through the entire array, rendering a button for each index of the array
     for (var i = 0; i < giphs.length; i++) {
         console.log("first", giphs[i]);
-        //dynamically creating a button through JQuery with a css class for styling
-        var $btn = $('<button>').css({"margin-top": "5px"});
+        //dynamically creating a button through JQuery with css styling to prevent each button from touching one another
+        var $btn = $('<button>').css({"margin": "5px 5px"});
         console.log("second", $btn);
         //adding the class="giphs" to the newly dynamically created buttons
         $btn.addClass('giphs');
@@ -40,7 +49,7 @@ function makeButtons() {
         $btn.text(giphs[i]);
         console.log('fifth', $btn);
         //locates each button in the div with the id="giph-view"
-        $('#giph-view').append($btn);
+        $('#giph-buttons').append($btn);
         console.log("sixth", $btn);
         }
     }
@@ -49,11 +58,20 @@ $("#add-giph").on('click', function (event){
     console.log('click', event);
     /*this function prevents the page from refreshing & duplicating the current buttons 
         each time the Add A Giph/submit button is clicked*/
-    event.preventDefault()
-
+    event.preventDefault();
+    //this var holds the text label as entered into the text box input area, trimming off extra white space
     var giph = $('#giph-input').val().trim();
+    console.log(giph);
+    //this function adds the text from the text box input area to the giphs array
+    giphs.push(giph);
+
+    makeButtons();
 
 })
+
+$(document).on('click', '.giphs', playGiph);
+    console.log(playGiph);
+
 makeButtons();
 
 });
