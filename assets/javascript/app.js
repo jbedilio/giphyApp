51cdbb947b486c5f85c5fbfce4a529f23fd5b133
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-02 10:54:01 -0400

    fixed the event listener and added preventDefault function to stop screen refreshing on each click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
 var giphs = [ "puppy"]; //"dog", "kitten", "cat", "hamster", "bird", "turtle", "rabbit", "horse", "goat"];
 
 function showGiph() {
-    var animal = $(this).attr('data-name')
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=1e1310e23de94135a921363415524870&limit=10";
+    //var that holds the button/element value clicked on and concatenates into the queryURL var completing the ajax request
+    var giph = $(this).attr('data-name')
+    //var holding the incomplete ajax request, waiting on the giph var to concatenate and complete the request
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giph + "&api_key=1e1310e23de94135a921363415524870&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -31,7 +33,7 @@ function makeButtons() {
         //adding the class="giphs" to the newly dynamically created buttons
         $btn.addClass('giphs');
         console.log("third", $btn);
-
+        //adding the data-name attribute to the buttons
         $btn.attr('data-name', giphs[i]);
         console.log("forth", $btn);
         //gives each button a text lable matching the text of the element in the array
@@ -42,7 +44,16 @@ function makeButtons() {
         console.log("sixth", $btn);
         }
     }
+//this functin adds a click event listener to the Add A Giph button
+$("#add-giph").on('click', function (event){
+    console.log('click', event);
+    /*this function prevents the page from refreshing & duplicating the current buttons 
+        each time the Add A Giph/submit button is clicked*/
+    event.preventDefault()
 
+    var giph = $('#giph-input').val().trim();
+
+})
 makeButtons();
 
 });
